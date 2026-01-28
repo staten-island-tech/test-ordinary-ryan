a28821550dce0bd57ad6b38f9f38d9862a79b3ef
ordinary-ryan <ryanl422@nycstudents.net>
2026-01-28 10:16:32 -0500

    day 2

---
+++ work.py
@@ -0,0 +1,26 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+""" t.forward(200) """
+
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200) """
+
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200)
+turtle.done()
