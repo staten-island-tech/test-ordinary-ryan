c0289741944654284c484c62a4d7649367c50452
ordinary-ryan <ryanl422@nycstudents.net>
2026-01-29 10:17:25 -0500

    day3

---
+++ day3.py
@@ -0,0 +1,19 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+for i in range(60):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90) 
+    t.right(5) 
+
+""" for i in range(3):
+    t.forward(100)
+    t.left(120) """
+turtle.done()
--- work.py
+++ work.py
@@ -16,11 +16,39 @@ t = Turtle()
 square(200) """
 
 
-def equal(x):
+""" def equal(x):
     t.forward(x)
+     t.left(120)
+     t.forward(x)
+     t.left(120)
+     t.forward(x)
+ equal(200) """
+
+""" def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right() """
+
+""" def rectangle():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+rectangle() """
+
+""" def equal():
+    t.forward(90)
     t.left(120)
-    t.forward(x)
+    t.forward(90)
     t.left(120)
-    t.forward(x)
-equal(200)
+    t.forward(90)
+equal() """
+
+
 turtle.done()
