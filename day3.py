5c7a107b0ca4fb8f27959d979d28a8c978534234
ordinary-ryan <ryanl422@nycstudents.net>
2026-01-30 10:17:39 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -2,7 +2,7 @@ import turtle
 from turtle import *
 t = Turtle()
 
-for i in range(60):
+""" for i in range(60):
     t.forward(100)
     t.left(90)
     t.forward(100)
@@ -12,8 +12,32 @@ for i in range(60):
     t.forward(100)
     t.left(90) 
     t.right(5) 
-
+ """
 """ for i in range(3):
     t.forward(100)
     t.left(120) """
+""" 
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90)
+ """
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(iRange):
+        t.forward(x)
+        t.left(y)
+square(100,90)
+
+def doubleSquare(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length,90)
+        length = length * 2
+    doubleSquare(5)
+
 turtle.done()
