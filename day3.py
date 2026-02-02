5e49a27a31ed60d68ac30a7a15b590bc4b28a065
ordinary-ryan <ryanl422@nycstudents.net>
2026-02-02 10:17:19 -0500

    Update day3.py

--- day3.py
+++ day3.py
@@ -25,19 +25,45 @@ def square(x,y):
         t.left(y)
 square(100,90)
  """
-sidelength = 100
-rotate = 90
-def square(x,y):
-    for i in range(iRange):
+""" def square(x,y):
+    for i in range(4):
         t.forward(x)
         t.left(y)
-square(100,90)
 
-def doubleSquare(iRange):
+def doubleSquares(iRange):
     length = 25
-    for i in range(iRange):
+    for i in range(iRange):         
         square(length,90)
         length = length * 2
-    doubleSquare(5)
+doubleSquares(5) """
+""" def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
 
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 25
+addSquares(5) """
+
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.right(5)
+
+def addSquares(iRange):
+    length = 5
+    for i in range(iRange):
+        square(length, 90)
+        length += 5  
+    addSquares(60)
+        
 turtle.done()
